e5c39c4185e01108baa3ce35520e79a42d10b07e
Cristian Beltran <cbeltranc6@gmail.com>
2019-08-24 16:39:28 -0400

    Inicio Backend API-REST: conexion a MongoDB y Servidor Express

---
+++ servidor_api-rest/config/config.js
@@ -0,0 +1,3 @@
+module.exports.URL_MONGO = 'mongodb://ficha:fichaclinica2019@ds211368.mlab.com:11368/ficha-clinica';        // Exporta la constande de las Credenciales del servidor de MongoDB y su direccion
+
+module.exports.PUERTO = process.env.PORT || 3000;  // Puerto en que escucha las peticiones al servidor
