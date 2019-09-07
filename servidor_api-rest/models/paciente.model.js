e510e07b19bf6bfc4b00c32843ff12f41975c3c4
Alex Martinez Neira <alex.martinez2017@alu.uct.cl>
2019-09-06 22:31:43 -0400

    Creacion de los Modelos
    Modelo Paciente, Modelo Ficha

---
+++ servidor_api-rest/models/paciente.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pacienteSchema = new Schema({
+    nombre: { type: String, required: [true, "El nombre del paciente debe ser completo"] },
+    rut: { type: String, required: [true, "Sin digito verificador"] },
+    fecha_nacimiento: { type: String, default: 0 },
+    sexo: { type: String, required: [true, "Hombre o Mujer"] },
+    direccion: { type: String, required: [true, "Direccion actual del Paciente"] },
+    prevision: { type: String, required: [true, "Fonasa o Insapre"] },
+    fono: { type: String, required: [true, "Telefono o celular"] }
+});
+
+module.exports = mongoose.model('Paciente', pacienteSchema); //Exportar el Modulo
--- servidor_api-rest/models/usuario.model.js
+++ servidor_api-rest/models/usuario.model.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const Schema = mongoose.Schema;   // 
+const Schema = mongoose.Schema; // 
 
-const usuarioSchema = new Schema({      // datos de objeto usuario permitidos
-    nombre: {type: String, required: [true, "El nombre del usuario es obligatorio"]},
-    email: {type: String, unique:true, required: [true, "El email es obligatorio"]},
-    password: {type: String, required: [true, "La contraseña es obligatoria"]},
-    token2FA: {type: String, default:''},      
-    Activo2FA: {type: Boolean, default: false},
-    activo: {type: Boolean, default: false },
+const usuarioSchema = new Schema({ // datos de objeto usuario permitidos
+    nombre: { type: String, required: [true, "El nombre del usuario es obligatorio"] },
+    email: { type: String, unique: true, required: [true, "El email es obligatorio"] },
+    password: { type: String, required: [true, "La contraseña es obligatoria"] },
+    token2FA: { type: String, default: '' },
+    Activo2FA: { type: Boolean, default: false },
+    activo: { type: Boolean, default: false },
     roles: []
 });
 
 usuarioSchema.plugin(uniqueValidator, { message: 'El {PATH} tiene que ser unico' });
 
-module.exports = mongoose.model('Usuario', usuarioSchema);          // Exporta el Modelo
+module.exports = mongoose.model('Usuario', usuarioSchema); // Exporta el Modelo
